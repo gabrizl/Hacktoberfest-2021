741fd784a15dbaaaf6e382218cab8a5378b0bd5a
JabutideLucas <91763305+JabutideLucas@users.noreply.github.com>
2021-10-01 19:57:24 -0300

    Create 3038.c

---
+++ 3038.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+
+    char string[300];
+
+    while (~scanf("%[^\n]%*c", string))
+    {
+
+        for (int i = 0; string[i]; ++i)
+            if (string[i] == '@')
+                putchar_unlocked('a');
+            else if (string[i] == '&')
+                putchar_unlocked('e');
+            else if (string[i] == '!')
+                putchar_unlocked('i');
+            else if (string[i] == '*')
+                putchar_unlocked('o');
+            else if (string[i] == '#')
+                putchar_unlocked('u');
+            else
+                putchar_unlocked(string[i]);
+        
+        putchar_unlocked('\n');
+
+    }
+
+    return 0;
+
+}
