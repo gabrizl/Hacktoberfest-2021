cf00dd17ec094cb00d556ff8419785302d72a68f
Mariane Zeitouni <marianezei@gmail.com>
2021-10-01 14:55:17 -0300

    register time online

---
+++ OnlineRegister.java
@@ -0,0 +1,38 @@
+
+public class OnlineRegister{
+    private String nomeDisc;
+    private int tempoE;
+    private int totalTempoOn;
+    
+    
+    public OnlineRegister(String nomeDisciplina){
+        nomeDisc = nomeDisciplina;
+        tempoE = 120;
+    }
+
+    
+    public OnlineRegister(String nomeDisciplina, int tempoOnlineEsperado){
+        nomeDisc = nomeDisciplina;
+        tempoE = tempoOnlineEsperado;        
+    }
+
+    
+    
+    public void adicionaTempoOnline(int tempo){
+        totalTempoOn += tempo;
+    }
+        
+    
+    public boolean atingiuMetaTempoOnline(){
+        if(totalTempoOn >= tempoE){
+            return true;
+        }else{
+            return false;
+        }   
+    }
+
+    
+    public String toString(){
+        return nomeDisc + " " + Integer.toString(totalTempoOn) + "/" + Integer.toString(tempoE);
+    }
+}
